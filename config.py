b560a809a24e7beab4a79ed5bffbb41dd4f171b6
Rafal-ali <nwr517654@gmail.com>
2026-03-01 17:59:56 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Flask and DB config
-SECRET_KEY = 'your-secret-key'
-# Use a database path relative to the app directory
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'app', 'database', 'database.db')}"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-LOGIN_DISABLED = False
